cd5c397b150273ae40ed9cdfb3d381185eee5521
Eric Slutz <eric.slutz@icloud.com>
2023-01-03 00:13:04 -0500

    initial portfolio website commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { RecognitionComponent } from './recognition/recognition.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'achievements', component: RecognitionComponent},
+  { path: 'projects', component: ProjectsComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/recognition/recognition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recognition',
+  templateUrl: './recognition.component.html',
+  styleUrls: ['./recognition.component.css']
+})
+export class RecognitionComponent {
+
+}
