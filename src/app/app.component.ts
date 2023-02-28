4f21765f15e1d1e14969e349cec0aa65bcb4c68d
Eric Slutz <eric.slutz@icloud.com>
2023-02-28 15:46:18 -0500

    added google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Title } from '@angular/platform-browser';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { filter, map } from 'rxjs';
 
+declare const gtag: Function;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,30 +15,36 @@ export class AppComponent {
   makeItStick: boolean = false;
   mobileNav: boolean = false;
 
-  constructor(private titleService:Title, private router: Router) {
-  }
+  constructor(private titleService: Title, private router: Router) {}
 
   ngOnInit() {
     this.router.events
-    .pipe(
-      filter((event) => event instanceof NavigationEnd),
-      map(() => {
-        let route: ActivatedRoute = this.router.routerState.root;
-        let routeTitle = '';
-        while (route!.firstChild) {
-          route = route.firstChild;
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route: ActivatedRoute = this.router.routerState.root;
+          let routeTitle = '';
+          while (route!.firstChild) {
+            route = route.firstChild;
+          }
+          if (route.snapshot.data['title']) {
+            routeTitle = route!.snapshot.data['title'];
+          }
+          return routeTitle;
+        })
+      )
+      .subscribe((title: string) => {
+        if (title) {
+          this.titleService.setTitle(`Eric Slutz | ${title}`);
         }
-        if (route.snapshot.data['title']) {
-          routeTitle = route!.snapshot.data['title'];
-        }
-        return routeTitle;
-      })
-    )
-    .subscribe((title: string) => {
-      if (title) {
-        this.titleService.setTitle(`Eric Slutz | ${title}`);
-      }
-    });
+      });
+
+      this.router.events
+        .subscribe((event) => {
+          if (event instanceof NavigationEnd) {
+            gtag('config', 'G-9YBST1VWZJ', { 'page_path': event.urlAfterRedirects });
+          }
+      });
   }
 
   @HostListener('window:scroll', ['$event'])
